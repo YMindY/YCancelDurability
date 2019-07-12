63e1cc71d99bd35e745fd9ae579912595615329e
YxMingY <526817087@qq.com>
2019-07-12 18:17:03 +0800

    改用nbt方式

--- src/yxmingy/canceldurability/Main.php
+++ src/yxmingy/canceldurability/Main.php
@@ -2,14 +2,15 @@
 
 namespace yxmingy\canceldurability;
 
-use pocketmine\Player;
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityDamageByEntityEvent;
-use pocketmine\event\player\PlayerItemHeldEvent;
-use pocketmine\event\player\PlayerInteractEvent;
 use pocketmine\plugin\PluginBase;
 use yxmingy\canceldurability\starter\Starter;
 use pocketmine\event\Listener;
+use pocketmine\item\Item;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\event\entity\EntityInventoryChangeEvent;
 
 class Main extends PluginBase implements Listener
 {
@@ -22,66 +23,54 @@ class Main extends PluginBase implements Listener
   public function onEnable()
   {
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
-    $this->getServer()->getScheduler()->scheduleRepeatingTask(new FixTask($this), 10*20);
     self::notice("[".self::PLUGIN_NAME."] is Enabled by xMing!");
   }
   public function onDisable()
   {
     self::warning("[".self::PLUGIN_NAME."] is Turned Off.");
   }
-  public static function repairClothes(Player $player)
+  
+  public static function setItemUnbreakable(Item &$item)
   {
-    $inventory = $player->getInventory();
-    $armors = $inventory->getArmorContents();
-    for ($i=0;$i<4;$i++) {
-      $armors[$i]->setDamage(0);
-      $inventory->setArmorItem($i, $armors[$i]);
+    if($item->isArmor() || $item->isTool()) {
+      $tag = $item->getNamedTag();
+      if($tag == null) {
+        $tag = new CompoundTag("",[
+          new IntTag("Unbreakable",1)
+        ]);
+        $item->setNamedTag($tag);
+      }elseif(($utag=$item->getNamedTagEntry("Unbreakable") !== null) && $utag <= 0) {
+        $tag->offsetSet("Unbreakable", 1);
+        $item->setNamedTag($tag);
+      }
     }
   }
-  public static function repairItemInHand(Player $player)
+  public static function setAllUnbreakable(Player $player)
   {
     $inventory = $player->getInventory();
-    $item = $inventory->getItemInHand();
-    if($item->isTool() || $item->isArmor()){
-      $item->setDamage(0);
-      $inventory->setItemInHand($item);
-    }
-  }
-  public static function repairInventory(Player $player)
-  {
-    $inventory = $player->getInventory();
+    $items = $inventory->getContents();
     for($i=0;$i<$inventory->getMaxStackSize();$i++) {
-      $item = $inventory->getItem($i);
-      if($item->isTool() || $item->isArmor()){
-        $item->setDamage(0);
-        $inventory->setItem($i, $item);
+      if(isset($items[$i])) self::setItemUnbreakable($items[$i]);
+    }
+    $inventory->setContents($items);
+    $items = $inventory->getArmorContents();
+    for($i=0;$i<4;$i++) {
+      if(isset($items[$i])) self::setItemUnbreakable($items[$i]);
+    }
+    $inventory->setArmorContents($items);
+  }
+  public function onJoin(PlayerJoinEvent $event)
+  {
+    self::setAllUnbreakable($event->getPlayer());
+  }
+  public function onInventoryChange(EntityInventoryChangeEvent $event) {
+    if($event->getEntity() instanceof Player) {
+      $item = $event->getNewItem();
+      self::setItemUnbreakable($item);
+      if($item->isArmor() || $item->isTool()) {
+        $event->setNewItem($item);
       }
     }
   }
-  public static function repairAll(Player $player) {
-    self::repairClothes($player);
-    self::repairInventory($player);
-    self::repairItemInHand($player);
-  }
-  public function onDamage(EntityDamageEvent $event)
-  {
-    if($event instanceof EntityDamageByEntityEvent) {
-      $damager = $event->getDamager();
-      $hurter = $event->getEntity();
-      if($damager instanceof Player) {
-        self::repairItemInHand($damager);
-      }
-      if($hurter instanceof Player) {
-        self::repairClothes($hurter);
-        self::repairItemInHand($hurter);
-      }
-    }
-  }
-  public function onItemHeld(PlayerItemHeldEvent $event)
-  {
-    self::repairItemInHand($event->getPlayer());
-  }
-  public function onTouch(PlayerInteractEvent $event) {
-    self::repairItemInHand($event->getPlayer());
-  }
+
 }
